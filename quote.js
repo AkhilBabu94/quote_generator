687d3662978e629ebf1855e015844b261e3c63d2
AkhilBabu94 <akhilbabu94@gmail.com>
2018-06-10 19:17:34 +0530

    quote.js

--- quote.js
+++ quote.js
@@ -6,9 +6,10 @@ $(document).ready(function() {
 });
 
 function getQuote() {
+  
   $.getJSON(url, function(data) {
-    $(".quote").html(data.quoteText);
-    $(".author").html(data.quoteAuthor);
+    $(".text").html(data.quoteText);
+    $(".author").html("-"+data.quoteAuthor);
 var num = "#" +Math.random().toString(16).slice(2,8);
     $("body").css("background-color", num);
     $(".quote-text").css("color",num);
@@ -17,9 +18,12 @@ var num = "#" +Math.random().toString(16).slice(2,8);
 $("#new-quote").on("click", function() {
   getQuote();
 });
-$("#tweet-quote").on("click", function(){
-                  window.open("https://twitter.com/intent/tweet?hashtags=&text=" + data.quoteText + ' -' + data.quoteAuthor);
+
+$("#tweet-quote").on("click", function(){   $("#tweet-quote").attr('href',"https://twitter.com/intent/tweet?hashtags=&text=" + data.quoteText + ' -' + data.quoteAuthor);
 });
-  });
-}
+});
+  }
+
+ 
+  
 
